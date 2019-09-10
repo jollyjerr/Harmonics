668db5038fdceaee51ed14ab85d4ce894e2342a6
jollyjerr <jollyjerr@gmail.com>
2019-09-10 10:07:31 -0600

    adds full crud on phrases

---
+++ harmonicsBackend/app/controllers/phrases_controller.rb
@@ -0,0 +1,40 @@
+class PhrasesController < ApplicationController
+    def index
+        phrases = Phrase.all
+        render json: phrases
+    end
+
+    def show
+        phrase = Phrase.all.find(params[:id])
+        render json: phrase
+    end
+
+    def create
+        phrase = Phrase.create(phrase_params)
+        if phrase.save
+            render json: phrase
+        else    
+            puts phrase
+        end
+    end
+
+    def update
+        phrase = Phrase.all.find(params[:id])
+        phrase.update(phrase_params)
+        if phrase.save
+            render json: phrase
+        else    
+            puts phrase
+        end
+    end
+
+    def destroy
+        Phrase.find(params[:id]).destroy
+    end
+
+    private
+
+    def phrase_params
+        params.require(:phrase).permit(:name, :description, :content)
+    end
+end
---
+++ harmonicsBackend/db/migrate/20190910145218_create_phrases.rb
@@ -0,0 +1,11 @@
+class CreatePhrases < ActiveRecord::Migration[5.2]
+  def change
+    create_table :phrases do |t|
+      t.string :name
+      t.string :description
+      t.string :content
+
+      t.timestamps
+    end
+  end
+end
