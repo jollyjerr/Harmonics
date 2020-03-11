42a69bec5cb7cf17772ef7387f730c2618363b89
jollyjerr <jollyjerr@gmail.com>
2020-03-10 19:30:50 -0600

    :art: Add styling setup

---
+++ src/components/Home.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Shell } from "../assets/styles";
+import Typography from "@material-ui/core/Typography";
+
+const Home = () => {
+    return (
+        <Shell>
+            <Typography>Home</Typography>
+        </Shell>
+    );
+};
+
+export default Home;
---
+++ src/components/NotFound.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Shell } from "../assets/styles";
+import Typography from "@material-ui/core/Typography";
+
+const NotFound = () => {
+    return (
+        <Shell>
+            <Typography>Not Found</Typography>
+        </Shell>
+    );
+};
+
+export default NotFound;
