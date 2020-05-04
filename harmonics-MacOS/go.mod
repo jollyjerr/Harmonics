8763bd28ef743b0392dc4c433c0742ee0b37be17
jollyjerr <jollyjerr@gmail.com>
2020-05-03 20:05:40 -0600

    :sparkles: add macos native build

---
+++ harmonics-MacOS/go.mod
@@ -0,0 +1,8 @@
+module harmonics
+
+go 1.12
+
+require (
+	github.com/gobuffalo/packr v1.30.1
+	github.com/zserge/webview v0.0.0-20200410160118-50e4bcc420ab
+)
--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,4 +1,5 @@
 import "@testing-library/jest-dom";
+import "@testing-library/jest-dom/extend-expect";
 
 import { configure } from "@testing-library/react";
 
